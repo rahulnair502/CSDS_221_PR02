4d370e3df844389a0f0343a4a0b395bae400a22a
rahulnair502 <71089258+rahulnair502@users.noreply.github.com>
2022-11-16 20:57:23 -0500

    added bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 const { createApp } = require('vue');
-import App from "./App.vue";
 
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 createApp(App).mount("#app");
