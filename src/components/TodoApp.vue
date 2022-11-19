c5359dce8c7866758d4359fb6f3382e874a7fba6
rahulnair502 <71089258+rahulnair502@users.noreply.github.com>
2022-11-18 19:33:44 -0500

    added bootstrap modal

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,102 +1,147 @@
 <template>
-
   <div class="container" style="max-width: 600px">
     <!-- Heading -->
 
-    <div class="card  text-light bg-primary">
-      <div class=" card-body text-center">
-        
-          <div class="fa-solid fa-bars d-inline-block "></div>
-          <div class = "d-inline-block  "> FRAMEWORKS</div>
-            <a-button type="primary d-inline-block float-end" @click="showModal"> <i class="fa-solid fa-circle-plus"></i>  Add </a-button>
-        <div>
-  
+    <div class="card text-light bg-primary">
+      <div class="card-body text-center">
+        <div class="fa-solid fa-bars d-inline-block"></div>
+        <div class="d-inline-block">FRAMEWORKS</div>
+        <!-- Button trigger modal -->
+        <button
+          type="button"
+          class="btn btn-primary float-end"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          <i class="fa-solid fa-circle-plus"></i> Add
+        </button>
+        <div></div>
+      </div>
+    </div>
   </div>
+
+  <!-- Task table -->
+  <table class="table table-hover mt-3">
+    <thead>
+      <tr class="text-center">
+        <td scope="col">Title</td>
+        <td scope="col">Description</td>
+        <td scope="col">Deadline</td>
+        <td scope="col">Priority</td>
+        <td scope="col">Is Complete</td>
+        <td scope="col">Action</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(task, index) in tasks" :key="index">
+        <td>{{ task.Title }}</td>
+        <td>{{ task.Description }}</td>
+        <td>{{ task.Deadline }}</td>
+        <td>{{ task.Priority }}</td>
+        <td class="text-center">
+          <div>
+            <input
+              class="form-check-input"
+              v-on:click="isHiddenCheck = !isHiddenCheck"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+          </div>
+        </td>
+        <td class="text-center">
+          <div class="span2">
+            <button
+              v-if="!isHiddenCheck"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+              class="text-light btn btn-sm btn-block bg-primary"
+            >
+              <i class="fa-solid fa-pen-to-square text-light"></i> Update
+            </button>
+            <button
+              @click="deleteTask(index)"
+              class="text-light btn btn-sm btn-block bg-danger"
+            >
+              <i class="fa-solid fa-circle-xmark text-light"></i> Delete
+            </button>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header bg-primary text-center text-light">
+          <h5 class="modal-title text-light text-center" id="exampleModalLabel">
+            <i class="fa-solid fa-circle-plus"></i> Add Task
+          </h5>
+        </div>
+
+        <div class="modal-body">
+          <div class="input-group mb-3">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Title"
+              aria-label="Title"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+
+          <div class="input-group mb-3">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Description"
+              aria-label="Description"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          <div class="input-group date" data-provide="datepicker">
+            <input
+              class="form-control py-2 border-right-0 border"
+              type="search"
+             
+              id="date"
+            />
+            <span class="input-group-append">
+              <div class="input-group-text bg-transparent">
+                <i class="fa fa-calendar"></i>
+              </div>
+            </span>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
     </div>
-
-    <!-- Task table -->
-    <table class="table table-hover mt-3">
-      <thead>
-        <tr class = text-center>
-          <td scope="col">Title </td>
-          <td scope="col">Description</td>
-          <td scope="col">Deadline</td>
-          <td scope="col">Priority</td>
-          <td scope="col">Is Complete</td>
-          <td scope="col">Action</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(task, index) in tasks" :key="index">
-          <td>{{ task.Title }}</td>
-          <td>{{ task.Description }}</td>
-          <td>{{ task.Deadline }}</td>
-          <td>{{ task.Priority }}</td>
-          <td class="text-center">
-            <div>
-              <input
-                class="form-check-input"
-                v-on:click="isHiddenCheck = !isHiddenCheck"
-                type="checkbox"
-                value=""
-                id="flexCheckDefault"
-              />
-            </div>
-          </td>
-        <td class="text-center">
-        
-         <div class="span2">
-
-  
-            <button v-if="!isHiddenCheck"  @click="showModal" class=" text-light btn btn-sm btn-block bg-primary"><i class="fa-solid fa-pen-to-square text-light"></i> Update</button>
-            <button @click="deleteTask(index)" class=" text-light btn btn-sm btn-block  bg-danger"><i class="fa-solid fa-circle-xmark text-light"></i> Delete  </button>
-        
-        </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-
-  <!--Modal-->
-
-  <a-modal v-model:visible="visible" wrap-class-name="full-modal-to-xl">
-  
-       <template #footer>
-   
-        <a-button key="submit"  :loading="loading" @click="Task"
-             class="btn btn-sm btn-block  bg-primary text-light"><i class="fa-solid fa-circle-plus text-light"></i> ADD </a-button>
-         <a-button key="back"  :loading="loading" @click="Task"
-             class="btn btn-sm btn-block  bg-danger text-light"><i class="fa-solid fa-circle-xmark text-light"></i> CANCEL</a-button>
-      
-      </template>  
-      
-    <p>Some contents...</p>
-    <p>Some contents...</p>
-    <p>Some contents...</p>
-
-    
-  </a-modal>
+  </div>
 </template>
 
 <script>
-import { ref, defineComponent } from 'vue';
-
-export default defineComponent({
-  setup() {
-    const visible = ref(false);
-
-    const showModal = () => {
-      visible.value = true;
-    };
-    return { visible, showModal };
-  },
-
-   data() {
-    
+export default {
+  data() {
     return {
       task: '',
+      date: null,
       editedTask: null,
       show: false,
       isHiddenCheck: false,
@@ -108,18 +153,16 @@ export default defineComponent({
           Priority: 'Low',
         },
       ],
-      
     };
   },
   methods: {
-
-     closeModal() {
+    closeModal() {
       this.show = false;
-      document.querySelector("body").classList.remove("overflow-hidden");
+      document.querySelector('body').classList.remove('overflow-hidden');
     },
     openModal() {
       this.show = true;
-      document.querySelector("body").classList.add("overflow-hidden");
+      document.querySelector('body').classList.add('overflow-hidden');
     },
     /**
      * Capitalize first character
@@ -167,5 +210,5 @@ export default defineComponent({
       this.task = '';
     },
   },
-});
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 const { createApp } = require('vue');
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
-
+import "bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css"
+import "bootstrap-datepicker"
 createApp(App).use(Antd).mount("#app");
